0fba3d32e61c7a1c72861557bf005c4685e90544
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-04-08 13:51:39 +0200

    Initial commit

---
+++ php/snack1.php
@@ -0,0 +1,66 @@
+<?php /*
+	Create an array with 15 basketball games. Each games is an array of home and away team. Each team is an array of name and score. every score is different.
+*/
+//Create array
+$basketballGames = [
+	[
+		"home" => [
+			"name" => "Lakers",
+			"score" => 100
+		],
+		"away" => [
+			"name" => "Warriors",
+			"score" => 120
+		]
+	],
+	[
+		"home" => [
+			"name" => "Celtics",
+			"score" => 96
+		],
+		"away" => [
+			"name" => "Spurs",
+			"score" => 85
+		]
+	],
+	[
+		"home" => [
+			"name" => "Rockets",
+			"score" => 137
+		],
+		"away" => [
+			"name" => "Jazz",
+			"score" => 122
+		]
+	],
+	[
+		"home" => [
+			"name" => "Heat",
+			"score" => 113
+		],
+		"away" => [
+			"name" => "Bulls",
+			"score" => 112
+		]
+	],
+	[
+		"home" => [
+			"name" => "Nets",
+			"score" => 102
+		],
+		"away" => [
+			"name" => "Cavaliers",
+			"score" => 98
+		]
+	],
+	[
+		"home" => [
+			"name" => "Clippers",
+			"score" => 107
+		],
+		"away" => [
+			"name" => "Mavericks",
+			"score" => 123
+		]
+	]
+];
