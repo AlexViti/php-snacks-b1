2e9032c09a60082a467719fcbf63985bd337175c
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-04-08 14:26:42 +0200

    Add snack 2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<h1>Snack 1</h1>
+	<h2>Snack 1</h2>
 	<?php include 'php/snack1.php' ?>
 	<ul>
 		<?php foreach ($basketballGames as $game) : ?>
@@ -16,5 +16,25 @@
 				<?php echo $game["home"]["name"] . "-" . $game["away"]["name"] . " | " . $game["home"]["score"] . " - " . $game["away"]["score"] ?>
 			</li>
 		<?php endforeach; ?>
+	</ul>
+	<h2>Snack 2</h2>
+
+	<form action="index.php" method="get">
+		<input type="text" name="name" placeholder="Name">
+		<input type="text" name="age" placeholder="Age">
+		<input type="text" name="email" placeholder="Email">
+		<button type="submit">Submit</button>
+	</form>
+	
+	<div class="result">
+		<?php if (isset($_GET["name"]) && isset($_GET["age"]) && isset($_GET["email"])) : ?>
+			<?php if (strlen($_GET["name"]) > 3 && is_numeric($_GET["age"]) && filter_var($_GET["email"], FILTER_VALIDATE_EMAIL)) : ?>
+				<p>Access granted.</p>
+			<?php else : ?>
+				<p>Access denied.</p>
+			<?php endif; ?>
+		<?php endif; ?>
+	</div>
+
 </body>
 </html>
